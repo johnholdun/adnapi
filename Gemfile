dfb794c9d1d489f1589bb1cf724b39c4dccf94fc
John Holdun <john@johnholdun.com>
2012-08-22 19:20:22 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source :gemcutter
+gem 'rack', '~>1.3.6'
+gem 'sinatra'
+gem 'nokogiri'
+
+group :development do
+  gem "tux"
+  gem "heroku"
+  gem "shotgun"
+end
---
+++ app.rb
@@ -0,0 +1,30 @@
+require 'open-uri'
+
+class App < Sinatra::Application
+  get '/' do
+    <<-form
+      <form action="/status">
+        <label for="url">URL</label>
+        <input type="text" id="url" name="url">
+        <button>Fetch</button>
+      </form>
+    form
+  end
+  
+  get '/status' do
+    result = nil
+    
+    doc = Nokogiri::HTML(open(params[:url])) rescue nil
+
+    if doc
+      user = doc.css('span.username').first.text
+      status = doc.css('span.post-content').text
+      
+      result = { user: user, status: status }
+    else
+      result = { error: true, body: open(params[:url]).read }
+    end
+    
+    [200, { 'Content-Type' => 'text/json' }, result.to_json]
+  end
+end
